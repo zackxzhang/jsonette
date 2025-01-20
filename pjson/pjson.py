4c8bda6c04026cdac3332a7799d5d7543e9c7294
Zack Zhang <zack.zhang.coder@gmail.com>
2025-01-20 12:12:25 -0500

    revamp python parser

--- pjson/pjson.py
+++ pjson/pjson.py
@@ -1,21 +1,15 @@
 # -*- coding: utf-8 -*-
 # json parsing in python
 
-from enum import Flag, auto
 
-
-class ArrayState(Flag):
-    COMMA = auto()
-    CLOSE = auto()
-    VALUE = auto()
-
-
-class ObjectState(Flag):
-    COLON = auto()
-    COMMA = auto()
-    CLOSE = auto()
-    VALUE = auto()
-    KEY   = auto()
+SPACES = ' \n\r\t'
+DECIMALS = '0123456789'
+HEXADECIMALS = DECIMALS + 'abcdefABCDEF'
+CONSTANTS = {
+    'null':  None,
+    'true':  True,
+    'false': False,
+}
 
 
 class Tape:
@@ -43,23 +37,23 @@ class Tape:
     def step(self, k=1):
         self.i += k
 
+    def skip(self):
+        while self.i < self.n and self.text[self.i] in SPACES:
+            self.i += 1
+
+    @property
+    def rear(self):
+        return self.i >= self.n
+
     def __getitem__(self, key):
         return self.text.__getitem__(key)
 
 
 class Parser:
 
-    SPACES = ' \n\r\t'
-    DIGITS = '0123456789'
-    CONSTANTS = {
-        'null':  None,
-        'true':  True,
-        'false': False,
-    }
-
     @property
     def const_inits(self):
-        return ''.join(key[0] for key in self.CONSTANTS.keys())
+        return ''.join(key[0] for key in CONSTANTS.keys())
 
     @property
     def num_inits(self):
@@ -67,7 +61,7 @@ class Parser:
 
     def read_constant(self, tape):
         i = tape.i
-        for const, value in self.CONSTANTS.items():
+        for const, value in CONSTANTS.items():
             k = len(const)
             if const == tape[i:i+k]:
                 tape.step(k)
@@ -80,31 +74,31 @@ class Parser:
         exponent = False
         c = tape.read()
         if c in '-123456789':
-            pass
+            tape.step()
         elif c == '0':
-            a = tape.look_ahead()
-            assert a in '.eE' or a in self.SPACES or a is None
+            tape.step()
+            a = tape.read()
+            assert a in '.eE' or a in SPACES or a is None
         else:
             raise ValueError('not a number')
-        tape.step()
         while True:
             try:
                 c = tape.read()
             except StopIteration:
                 break
-            if c in self.DIGITS:
+            if c in DECIMALS:
                 pass
             elif c == '.':
                 assert (
                     fraction is False and
                     exponent is False and
-                    tape.look_behind() in self.DIGITS
+                    tape.look_behind() in DECIMALS
                 )
                 fraction = True
             elif c in 'eE':
                 assert (
                     exponent is False and
-                    tape.look_behind() in self.DIGITS
+                    tape.look_behind() in DECIMALS
                 )
                 exponent = True
             elif c in '+-':
@@ -121,159 +115,111 @@ class Parser:
 
     def read_string(self, tape):
         start = tape.i
-        c = tape.read()
-        if c == '"':
-            tape.step()
-        else:
-            raise ValueError('not a string')
-        while True:
-            try:
+        assert tape.read() == '"', 'not a string'
+        tape.step()
+        try:
+            while True:
                 c = tape.read()
-            except StopIteration:
-                raise ValueError('string not enclosed')
-            tape.step()
-            if c == '\\':
-                if tape.read() in '"\\/bfnrt':
-                    tape.step()
-                elif tape.read() == 'u':
-                    tape.step()
-                    try:
-                        int(tape[tape.i:tape.i+4])
-                    except ValueError as exc:
-                        raise ValueError('bad hex digits') from exc
-                    tap.step(4)
-                else:
-                    raise ValueError('bad escape')
-            elif c == '"':
-                break
-        end=tape.i
+                tape.step()
+                if c == '\\':
+                    if tape.read() in '"\\/bfnrt':
+                        tape.step()
+                    elif tape.read() == 'u':
+                        tape.step()
+                        try:
+                            int(tape[tape.i:tape.i+4], 16)
+                        except ValueError as exc:
+                            raise ValueError('bad hexadecimals') from exc
+                        tap.step(4)
+                    else:
+                        raise ValueError('bad escape')
+                elif c == '"':
+                    break
+        except StopIteration:
+            raise ValueError('string not complete')
+        end = tape.i
         return tape[start+1:end-1]
 
     def read_array(self, tape):
         array = list()
-        c = tape.read()
-        if c == '[':
-            state = ArrayState.CLOSE | ArrayState.VALUE
-            tape.step()
-        else:
-            raise ValueError('not an array')
-        while True:
-            try:
-                c = tape.read()
-            except StopIteration:
-                raise ValueError('array not enclosed')
-            if c in self.const_inits:
-                assert ArrayState.VALUE in state
-                array.append(self.read_constant(tape))
-                state = ArrayState.COMMA | ArrayState.CLOSE
-            elif c in self.num_inits:
-                assert ArrayState.VALUE in state
-                array.append(self.read_number(tape))
-                state = ArrayState.COMMA | ArrayState.CLOSE
-            elif c == '"':
-                assert ArrayState.VALUE in state
-                array.append(self.read_string(tape))
-                state = ArrayState.COMMA | ArrayState.CLOSE
-            elif c == '[':
-                assert ArrayState.VALUE in state
-                array.append(self.read_array(tape))
-                state = ArrayState.COMMA | ArrayState.CLOSE
-            elif c == '{':
-                assert ArrayState.VALUE in state
-                array.append(self.read_object(tape))
-                state = ArrayState.COMMA | ArrayState.CLOSE
-            elif c in self.SPACES:
+        assert tape.read() == '[', 'not an array'
+        tape.step()
+        try:
+            tape.skip()
+            c = tape.read()
+            if c == ']':
                 tape.step()
-            elif c == ',':
-                assert ArrayState.COMMA in state
-                state = ArrayState.VALUE
-                tape.step()
-            elif c == ']':
-                assert ArrayState.CLOSE in state
-                tape.step()
-                break
             else:
-                raise ValueError('bad array')
+                while True:
+                    tape.skip()
+                    value = self.read_value(tape)
+                    array.append(value)
+                    tape.skip()
+                    c = tape.read()
+                    if c == ',':
+                        tape.step()
+                    elif c == ']':
+                        tape.step()
+                        break
+                    else:
+                        raise ValueError('array missing , or ]')
+        except StopIteration:
+            raise ValueError('array not complete')
         return array
 
     def read_object(self, tape):
         obj = dict()
-        key, value = None, None
-        state = ObjectState.CLOSE | ObjectState.KEY
-        c = tape.read()
-        if c == '{':
-            tape.step()
-        else:
-            raise ValueError('not an object')
-        while True:
-            try:
-                c = tape.read()
-            except StopIteration:
-                raise ValueError('object not enclosed')
-            if c in self.const_inits:
-                assert ObjectState.VALUE in state
-                value = self.read_constant(tape)
-                obj[key] = value
-                state = ObjectState.COMMA | ObjectState.CLOSE
-            elif c in self.num_inits:
-                assert ObjectState.VALUE in state
-                value = self.read_number(tape)
-                obj[key] = value
-                state = ObjectState.COMMA | ObjectState.CLOSE
-            elif c == '"':
-                if ObjectState.VALUE in state:
-                    value = self.read_string(tape)
-                    obj[key] = value
-                    state = ObjectState.COMMA | ObjectState.CLOSE
-                elif ObjectState.KEY in state:
-                    key = self.read_string(tape)
-                    state = ObjectState.COLON
-                else:
-                    assert ObjectState.VALUE in state or ObjectState.KEY in state
-            elif c == '[':
-                assert ObjectState.VALUE in state
-                value = self.read_array(tape)
-                obj[key] = value
-                state = ObjectState.COMMA | ObjectState.CLOSE
-            elif c == '{':
-                assert ObjectState.VALUE in state
-                value = self.read_object(tape)
-                obj[key] = value
-                state = ObjectState.COMMA | ObjectState.CLOSE
-            elif c in self.SPACES:
+        assert tape.read() == '{', 'not an object'
+        tape.step()
+        try:
+            tape.skip()
+            c = tape.read()
+            if c == '}':
                 tape.step()
-            elif c == ',':
-                assert ObjectState.COMMA in state
-                state = ObjectState.KEY
-                tape.step()
-            elif c == ':':
-                assert ObjectState.COLON in state
-                state = ObjectState.VALUE
-                tape.step()
-            elif c == '}':
-                assert ObjectState.CLOSE in state
-                tape.step()
-                break
             else:
-                raise ValueError('bad object')
+                while True:
+                    tape.skip()
+                    key = self.read_string(tape)
+                    tape.skip()
+                    assert tape.read() == ':', 'object missing :'
+                    tape.step()
+                    tape.skip()
+                    value = self.read_value(tape)
+                    obj[key] = value
+                    tape.skip()
+                    c = tape.read()
+                    if c == ',':
+                        tape.step()
+                    elif c == '}':
+                        tape.step()
+                        break
+                    else:
+                        raise ValueError('object missing , or }')
+        except StopIteration:
+            raise ValueError('object not complete')
         return obj
 
-    def __call__(self, text):
-        tape = Tape(text.strip(self.SPACES))
+    def read_value(self, tape):
         c = tape.read()
         if c in self.const_inits:
-            value = self.read_constant(tape)
+            return self.read_constant(tape)
         elif c in self.num_inits:
-            value = self.read_number(tape)
+            return self.read_number(tape)
         elif c == '"':
-            value = self.read_string(tape)
+            return self.read_string(tape)
         elif c == '[':
-            value = self.read_array(tape)
+            return self.read_array(tape)
         elif c == '{':
-            value = self.read_object(tape)
+            return self.read_object(tape)
         else:
-            raise ValueError('not a json')
-        assert tape[tape.i:].strip() == ''
+            raise ValueError('bad value')
+
+    def __call__(self, text):
+        tape = Tape(text)
+        tape.skip()
+        value = self.read_value(tape)
+        tape.skip()
+        assert tape.rear, 'bad remainder'
         return value
 
 
@@ -287,7 +233,7 @@ if __name__ == '__main__':
     print(parser.read_constant(Tape('true ,')))
     print(parser.read_string(Tape('"little"')))
     print(parser.read_string(Tape('"big",  0.9')))
-    print(parser.read_array(Tape('[0.9, true, ["hello", {"json":   1.2e-3}   ]]')))
+    print(parser.read_array(Tape('["\u0391", true, ["hello", {"json":   1.2e-3}   ]]')))
     print(parser.read_object(Tape('{"a": 1, "bcd":   {  "json": true, "xml": [null, 2e-3]}}')))
 
     examples = [
@@ -305,5 +251,3 @@ if __name__ == '__main__':
     ]
     for example in examples:
         print(parser(example))
-
-    # ToDo: custom exception, unit test
