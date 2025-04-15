6d0c4c185139335c8e81390408349083ed39cfb5
Zack Zhang <zack.zhang.coder@gmail.com>
2025-04-15 08:26:35 +0800

    add print and escape

--- hs/json.hs
+++ hs/json.hs
@@ -2,6 +2,7 @@
 
 
 import Control.Applicative (Alternative(..), optional)
+import Data.List (intercalate)
 
 
 data JValue = JNull
@@ -10,13 +11,41 @@ data JValue = JNull
             | JNumber { int :: Integer, frac :: Integer, expo :: Integer }
             | JArray [JValue]
             | JObject [(String, JValue)]
-            deriving (Eq, Show)
+            deriving (Eq)
 
--- instance Show JValue where
 
-newtype Parser i o = Parser { runParser :: i -> Maybe (i, o) }
+instance Show JValue where
+    show value = case value of
+        JNull         -> "null"
+        JBool   True  -> "true"
+        JBool   False -> "false"
+        JString s     -> show s
+        JNumber i 0 0 -> show i
+        JNumber i f 0 -> show i ++ "." ++ show f
+        JNumber i 0 e -> show i ++ "e" ++ show e
+        JNumber i f e -> show i ++ "." ++ show f ++ "e" ++ show e
+        JArray  a     -> "[" ++ intercalate ", " (map show a) ++ "]"
+        JObject o     -> "[" ++ intercalate ", " (map showKV o) ++ "]"
+        where
+            showKV (k, v) = show k ++ ": " ++ show v
 
--- lookahead :: Parser String Char
+
+escape :: Char -> Char
+escape c = case c of
+    '\\' -> '\\'
+    '"'  -> '"'
+    '/'  -> '/'
+    'b'  -> '\b'
+    'f'  -> '\f'
+    'n'  -> '\n'
+    'r'  -> '\r'
+    't'  -> '\t'
+    _    ->  c
+
+
+newtype Parser i o = Parser { parse :: i -> Maybe (i, o) }
+
+-- lookAhead :: Parser String Char
 
 -- jNull :: Parser String JValue
 
